8ad9dddf927962a683cfa430800530158da58d3b
RogalinPavel <pashtetsrogalikom@gmail.com>
2026-02-19 15:22:01 +0200

    Add files via upload

---
+++ lab2/index.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Привет, мир!<br />";
+
+echo "Hello, World with echo!<br />";
+print "Hello, World with print!<br />";
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+
+echo "Количество дней: " . $days . "<br />";
+echo $message . "<br />";
+
+
+echo "Количество дней: $days<br />";
+echo "$message<br />";
+?>
